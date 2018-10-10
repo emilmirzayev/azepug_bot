b5f5f90179d78644dd952a6dbdda80a392b054a1
emilmirzayev <mirzayev_emil@yahoo.com>
2018-10-10 22:29:03 +0200

    some changes

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
+# coding: utf8
+
+
 import time
 import telebot
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 
 
-bot = telebot.TeleBot(token="")
+bot = telebot.TeleBot(token="635974688:AAFC8IcXEkxI5g9VuRVGcn8U_v5dEyT_hzw")
 
 commands = {  # command description used in the "help" command
-              'start': 'Başlanğıc',
-              'help': 'Kömək',
+              'start': 'Salamlama',
+              'help': 'Mövcud əmrləri göstər',
               'kitab': 'Faydalı kitablar',
               'admin': 'Adminlə əlaqə',
               "video": "Faydalı videodərs pleylistləri"
@@ -22,23 +25,20 @@ def findat(msg):
         if '@' in i:
             return i
 
-def check_if_active(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+# def check_if_active(url):
+#     page = requests.get(url)
+#     soup = BeautifulSoup(page.text, 'html.parser')
 
-    if 'Page Not Found' in soup.title.text:
-        # soup.title.text gives the text of the <title> tag
-        return False
-    else:
-        return True
+#     if 'Page Not Found' in soup.title.text:
+#         # soup.title.text gives the text of the <title> tag
+#         return False
+#     else:
+#         return True
 
 @bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
-    bot.reply_to(message, """Salam, Azərbaycan Python istifadəçiləri qrupuna xoş gəlmisiniz. Mümkün əmr siyahısını görmək üçün /help yazın.
-                            Bizi fesbukda da izləyə bilərsiniz.\n
-                            https://www.facebook.com/groups/python.az
-                        
-                            """)
+    bot.reply_to(message, """Salam, Azərbaycan Python istifadəçiləri qrupuna xoş gəlmisiniz. Mümkün əmr siyahısını görmək üçün /help yazın. Bizi fesbukda da izləyə bilərsiniz.\n
+                            https://www.facebook.com/groups/python.az""")
 
 @bot.message_handler(commands=['help'])
 def command_help(message):
@@ -57,6 +57,13 @@ def tag_admin(message):
 @bot.message_handler(commands = ["kitab"])
 def book_suggestion(message):
     bot.reply_to(message, "Python öyrənmək üçün top 5 kitab:\n 1: Python Crash Course (https://nostarch.com/pythoncrashcourse) \n 2: Learn Python 3 the Hard Way (https://learnpythonthehardway.org/python3/) \n 3: Fluent Python: Clear, Concise, and Effective Programming (http://shop.oreilly.com/product/0636920032519.do) \n 4: Think Python: How to Think Like a Computer Scientist, 2nd edition (https://greenteapress.com/wp/think-python-2e/) \n 5: Rəsmi Python öyrədici materialları (https://docs.python.org/3/tutorial/index.html)")
+
+
+
+@bot.message_handler(commands = ["video"])
+def video_suggestion(message):
+    pass
+
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
